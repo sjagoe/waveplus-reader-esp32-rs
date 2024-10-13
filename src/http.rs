f94b9f320e29450b93d4d2509f276109ec35cd43
Simon Jagoe <simon@simonjagoe.com>
2024-10-13 18:01:45 +0300

    Move http get into new module

---
+++ src/http.rs
@@ -0,0 +1,74 @@
+use anyhow::{bail, Result};
+use core::str;
+use embedded_svc::{
+    http::{client::Client, Method},
+    io::Read,
+};
+use esp_idf_svc::{
+    http::client::{Configuration, EspHttpConnection},
+};
+
+pub fn get(url: impl AsRef<str>) -> Result<()> {
+    // 1. Create a new EspHttpClient. (Check documentation)
+    // ANCHOR: connection
+    let connection = EspHttpConnection::new(&Configuration {
+        use_global_ca_store: true,
+        crt_bundle_attach: Some(esp_idf_svc::sys::esp_crt_bundle_attach),
+        ..Default::default()
+    })?;
+    // ANCHOR_END: connection
+    let mut client = Client::wrap(connection);
+
+    // 2. Open a GET request to `url`
+    let headers = [("accept", "text/plain")];
+    let request = client.request(Method::Get, url.as_ref(), &headers)?;
+
+    // 3. Submit write request and check the status code of the response.
+    // Successful http status codes are in the 200..=299 range.
+    let response = request.submit()?;
+    let status = response.status();
+
+    println!("Response code: {}\n", status);
+
+    match status {
+        200..=299 => {
+            // 4. if the status is OK, read response data chunk by chunk into a buffer and print it until done
+            //
+            // NB. see http_client.rs for an explanation of the offset mechanism for handling chunks that are
+            // split in the middle of valid UTF-8 sequences. This case is encountered a lot with the given
+            // example URL.
+            let mut buf = [0_u8; 256];
+            let mut offset = 0;
+            let mut total = 0;
+            let mut reader = response;
+            loop {
+                if let Ok(size) = Read::read(&mut reader, &mut buf[offset..]) {
+                    if size == 0 {
+                        break;
+                    }
+                    total += size;
+                    // 5. try converting the bytes into a Rust (UTF-8) string and print it
+                    let size_plus_offset = size + offset;
+                    match str::from_utf8(&buf[..size_plus_offset]) {
+                        Ok(text) => {
+                            print!("{}", text);
+                            offset = 0;
+                        }
+                        Err(error) => {
+                            let valid_up_to = error.valid_up_to();
+                            unsafe {
+                                print!("{}", str::from_utf8_unchecked(&buf[..valid_up_to]));
+                            }
+                            buf.copy_within(valid_up_to.., 0);
+                            offset = size_plus_offset - valid_up_to;
+                        }
+                    }
+                }
+            }
+            println!("Total: {} bytes", total);
+        }
+        _ => bail!("Unexpected response code: {}", status),
+    }
+
+    Ok(())
+}
